8d0d9cb5a87b46c6d5548c7a600341f333f917d7
HardikGRG <Hardik Garg>
2023-02-18 10:54:20 +0530

    init

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const PORT = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') return respondHello(req, res)
-
+//sxxwsxwswsws
   res.end()
 })
-
+//hello
 function respondHello (req, res) {
   res.end(JSON.stringify({ msg: 'hello' }))
 }
-
+//hello
 server.listen(PORT)
 console.log(`Server listening on port ${PORT}`)
-
+//hey
 if (require.main !== module) module.exports = server
